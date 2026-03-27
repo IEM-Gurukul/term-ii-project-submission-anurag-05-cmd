bf8bcf2ba0247d75fc37c2cc50855d58332507fc
anurag-05-cmd <deb.anurag2005@gmail.com>
2026-03-27 23:43:10 +0530

    Initialised project structure and added abstract User class

---
+++ src/zipgo/model/User.java
@@ -0,0 +1,35 @@
+package zipgo.model;
+
+public abstract class User {
+    private String userId;
+    private String name;
+    private String phone;
+
+    public User(String userId, String name, String phone) {
+        this.userId = userId;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public abstract void displayInfo();
+}
