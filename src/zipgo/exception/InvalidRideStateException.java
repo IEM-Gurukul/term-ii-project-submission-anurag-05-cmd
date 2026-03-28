cc6d3eba1f54cf3819b8e8771cb19da46e26f8ee
anurag-05-cmd <deb.anurag2005@gmail.com>
2026-03-28 20:44:17 +0530

    Implemented custom exceptions for driver availability and ride state validation

---
+++ src/zipgo/exception/InvalidRideStateException.java
@@ -0,0 +1,8 @@
+package zipgo.exception;
+
+public class InvalidRideStateException extends Exception {
+
+    public InvalidRideStateException(String message) {
+        super(message);
+    }
+}
---
+++ src/zipgo/exception/NoDriverAvailableException.java
@@ -0,0 +1,8 @@
+package zipgo.exception;
+
+public class NoDriverAvailableException extends Exception {
+
+    public NoDriverAvailableException(String message) {
+        super(message);
+    }
+}
